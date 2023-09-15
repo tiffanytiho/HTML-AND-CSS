4b3fc6563f6389895f01ef9a740f0baa41999c5a
tiffanytiho <135434318+tiffanytiho@users.noreply.github.com>
2023-09-15 17:13:20 +0000

    EXERCERCICE 1
    CREATION DE 3 GRID

---
+++ exo1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>
+		
+	</title>
+	<link href="design.css" rel="stylesheet">
+</head>
+<body>
+	<main>
+		<div class="conteneur">
+		    <div class="box"> kjfhhhhhhhhh</div>
+		    <div class="box"> kjfhhhhhhhhhkj</div>
+		    <div class="box"> </div>
+		</div>
+	</main>
+
+</body>
+</html>
