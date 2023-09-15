f1f6ecfc7effad112506f07b4b4576fc6339c7b8
tiffanytiho <135434318+tiffanytiho@users.noreply.github.com>
2023-09-15 17:39:34 +0000

    EXERCICE 2
    AJOUTER DEUX LIGNES AU GRID (EN HAUT ET EN BAS)

---
+++ ex2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>
+		
+	</title>
+	<link href="design2.css" rel="stylesheet">
+</head>
+<body>
+	<main>
+		<div class="conteneur">
+			<div class="ligne-1">	</div>
+		    <div class="box"> 	</div>
+		    <div class="box"> 	</div>
+		    <div class="box"> </div>
+		    <div class="derniere">	</div>
+		</div>
+	</main>
+</body>
+</html>
